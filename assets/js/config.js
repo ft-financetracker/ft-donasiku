60d6ba3baeb7a7529776dc44e410d0dbb3b5e333
ft-financetracker <admin.financetracker@gmail.com>
2026-09-10 09:32:16 +0700

    Update config.js

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,1 +1,1 @@
-window.KIA_CONFIG = { VERSION:'0.1.0', API_URL:'PASTE_APPS_SCRIPT_WEB_APP_URL_HERE' };
+window.KIA_CONFIG = { VERSION:'0.1.0', API_URL:'https://script.google.com/macros/s/AKfycbz1YxRPn0DIuYEzJH1Y5o9yn_43xtB9n_ciCUAXzx950XvEayVS-hKiXUUoNTe0hEkCvA/exec' };
